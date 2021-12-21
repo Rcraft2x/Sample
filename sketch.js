394da651deab4afad793733b7f4a09d90c4fbaa1
Rcraft2x <96452632+Rcraft2x@users.noreply.github.com>
2021-12-20 20:13:11 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,23 +13,25 @@ function draw()
   if (keyIsDown(RIGHT_ARROW)) 
   {
     box.position.x = box.position.x +5;
+    background("blue")
   }
 
     if (keyIsDown(LEFT_ARROW)) 
   {
     box.position.x= box.position.x -5;
-    
+    background("red")
   }
  
     if (keyIsDown(UP_ARROW)) 
   {
     box.position.y = box.position.y - 5;
-   
+   background("green")
   }
 
   if (keyIsDown(DOWN_ARROW)) 
   {
     box.position.y = box.position.y + 5;
+    background("yellow")
   }
 
   drawSprites();
